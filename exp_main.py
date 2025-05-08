edbd16b1d02b7e73d0a6e5db3ca30f9984a2ffc7
dennis <dennisdust2@gmail.com>
2025-05-08 13:41:32 -0400

    Added an second if statement to check if user has more expenses they would want to add.

--- exp_main.py
+++ exp_main.py
@@ -20,43 +20,41 @@ def expenses_to_csv():
             try:
                 amount = float(input("Please enter the dollar amount (e.g. 1.12): "))
             except ValueError:
-                print("Invalid input...")
+                print("Invalid amount input...")
             else:
                 break
         
         category = input('Please enter the category of the expense (e.g. food, entertainment, transportation): ')
             
-        
-
         while True:
-            date = input('Please enter the date of the expense in DD/MM/YYYY format: ')
+            date = input('Please enter the date of the expense in MM/DD/YYYY format: ')
             date_pattern = r"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}$"
             if re.match(date_pattern, date):
                 break
             else:
                 print("Invalid date input...")
 
-        # date_pattern = r"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}$"
-        # if re.match(date_pattern, date):
-        #     return
-        # else:
-        #     print("Invalid date format")
 
         re_occuring = input('Is the expense re-occuring (y/n)? ')
-        print (expense_name,amount,category,date,re_occuring)
+        print (expense_name, amount, category, date, re_occuring)
+
         add_expense = input('Would you like to enter this expense (y/n)? ')
 
         if add_expense.lower() == 'y':
-            print('yes1')
-            w.writerow([expense_name, amount, date, re_occuring])
+            w.writerow([expense_name, amount, category, date, re_occuring])
+            print('Expense has been added...')
         elif add_expense.lower() == 'n':
             print('Expense has NOT been added...')
-        elif add_expense.lower() == 'stop':
-            print("Stoping...")
-            outfile.close
-            exit()
         else:
             print("Invalid entry, restarting...")
+        
+        
+        expense_continue = input('Do you have another expense to add (y/n)? ')
+        if expense_continue.lower() == 'y':
+            return
+        elif expense_continue.lower() == 'n':
+            print('Aborting program...')
+            exit()
 
     
 
